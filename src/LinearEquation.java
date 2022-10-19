aacd97a1c7fdfde8c06aaf16d8697967b8c4d359
Ryan Khan <ryank119@nycstudents.net>
2022-10-19 12:34:15 -0400

    lol

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,29 +1,55 @@
 public class LinearEquation {
-   private int x1;
-   private int y1;
-   private int x2;
-   private int y2;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
 
-   public LinearEquation (int x1, int y1, int x2, int y2) {
-       this.x1=x1;
-       this.y1=y1;
-       this.x2=x2;
-       this.y2=y2;
-   }
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
-   public String slope () {
-       int yNum= y2-y1;
-       int xNum= x2-x1;
-       double slope= (yNum)/(xNum);
-       if (yNum % xNum== 0) {
-           return (int) (slope) + "x";
-       } else {
-           return yNum + "/ " + xNum + "x";
-       }
+    public double roundedToHundredth(double toRound) {
+        double rounded = Math.round(toRound * 100.00) / 100.00;
+        return rounded;
+    }
 
-   }
+    public double slope() {
+        double slope = (double) (y2 - y1) / (double) (x2 - x1);
+        double roundedSlope = roundedToHundredth(slope);
+        return roundedSlope;
+    }
 
+    public double distance() {
+        double d = Math.pow((Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)), 0.5);
+        double roundedD = roundedToHundredth(d);
+        return roundedD;
+    }
 
+    public double yIntercept() {
+        double yIntercept = y2 - (slope() * x2);
+        double roundedYIINT = roundedToHundredth(yIntercept);
+        return roundedYIINT;
+    }
+
+    public String equation() {
+        int x = x2 - x1;
+        int y = y2 - y1;
+        if (x < 0) {
+            x = Math.abs(x);
+            y = -y;
+        }
+        String slope = y + "/" + x;
+
+        if (yIntercept() < 0) {
+            return "y= " + slope + "x " + "-" + Math.abs(yIntercept());
+        } else {
+            return "y= " + slope + "x " + Math.abs(yIntercept());
+        }
+
+    }
 
 
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -11,11 +11,15 @@ public class LinearEquationRunner {
         System.out.println();
         System.out.println("The two points are: " + coordinate1 + " and " + coordinate2);
 
-        int x1= Integer.parseInt(coordinate1.substring(0,2));
-        int y1= Integer.parseInt(coordinate1.substring(3,5));
-        int x2= Integer.parseInt(coordinate2.substring(0,2));
-        int y2= Integer.parseInt(coordinate2.substring(3,5));
-
+        int x1= Integer.parseInt(coordinate1.substring(1,coordinate1.indexOf(",")));
+        int y1= Integer.parseInt(coordinate1.substring(coordinate1.indexOf(",") + 1, coordinate1.indexOf(")")));
+        int x2= Integer.parseInt(coordinate2.substring(1,coordinate2.indexOf(",")));
+        int y2= Integer.parseInt(coordinate2.substring(coordinate2.indexOf(",") + 1,coordinate2.indexOf(")")));
         LinearEquation random= new LinearEquation(x1,y1,x2,y2);
+        String equation= random.equation();
+        System.out.println(equation);
+
+
+
     }
 }
