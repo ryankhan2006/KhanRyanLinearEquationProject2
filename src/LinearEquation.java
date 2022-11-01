6bb4311879ec254f6b51714d91ecde3c7038bed4
Ryan Khan <ryank119@nycstudents.net>
2022-11-01 17:29:35 -0400

    hello

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,10 +1,11 @@
 import static java.lang.Math.*;
-
+    //importing Math class
 public class LinearEquation {
     private int x1;
     private int y1;
     private int x2;
     private int y2;
+    //instance variables for the coordinates
 
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
@@ -12,77 +13,101 @@ public class LinearEquation {
         this.x2 = x2;
         this.y2 = y2;
     }
-
+//initializing the variables under the constructor
     public double roundedToHundredth(double toRound) {
         double rounded = Math.round(toRound * 100.00) / 100.00;
         return rounded;
     }
+    //rounding any number to the nearest hundredth
 
     public double slope() {
-        double slope = (double) (y2 - y1) / (double) (x2 - x1);
+        double slope = (double) (y2 - y1) / (x2 - x1);
         double roundedSlope = roundedToHundredth(slope);
         return roundedSlope;
     }
-
+// returns the slope rounded to the nearest hundredth
     public double distance() {
         double d = pow((pow(x2 - x1, 2)) + (pow(y2 - y1, 2)), 0.5);
         double roundedD = roundedToHundredth(d);
         return roundedD;
     }
-
+// returns the distance between two points rounded to the nearest hundredth
     public double yIntercept() {
         double yIntercept = (double) (y2 - (slope() * x2));
         double roundedYIINT = roundedToHundredth(yIntercept);
         return roundedYIINT;
     }
+    // return the y-Intercept rounded to the nearest hundredth
 
     public String equation() {
-        int y= y2-y1;
-        int x= x2-x1;
-        if (slope()==0.0) {
+        int y = y2 - y1;
+        int x = x2 - x1;
+        if (slope() == 1.0) {
             if (yIntercept() < 0) {
-                return "y = " + "-" + (int)(Math.abs(yIntercept()));
+                return "y = x - " + Math.abs(yIntercept());
+            } else if (yIntercept() > 0) {
+                return "y = x + " + yIntercept();
+            } else {
+                return "y = x";
             }
-            return "y = " + (int)(yIntercept());
         }
-        if (y%x==0) {
+        if (slope() == -1.0) {
             if (yIntercept() < 0) {
-                return "y = " + (y)/(x) + "x" + " - " + Math.abs(yIntercept());
-            } else if (yIntercept()==0) {
-                return "y = " + (y)/(x) + "x ";
+                return "y = -x - " + Math.abs(yIntercept());
+            } else if (yIntercept() > 0) {
+                return "y = -x + " + yIntercept();
+            } else {
+                return "y = -x";
             }
-            return "y = " + (y)/(x) + "x" + " + " + yIntercept();
         }
-        if (x<0) {
-            y=-(y);
-            x=Math.abs(x);
-            if (yIntercept()<0) {
-                return "y = " + y + "/" + x + "x " +  "- " + Math.abs(yIntercept());
-            } else if (yIntercept()==0) {
+
+            if (slope() == 0.0) {
+                if (yIntercept() < 0) {
+                    return "y = " + "-" + (int) (Math.abs(yIntercept()));
+                }
+                return "y = " + (int) (yIntercept());
+            }
+            if (y % x == 0) {
+                if (yIntercept() < 0) {
+                    return "y = " + (y) / (x) + "x" + " - " + Math.abs(yIntercept());
+                } else if (yIntercept() == 0) {
+                    return "y = " + (y) / (x) + "x ";
+                }
+                return "y = " + (y) / (x) + "x" + " + " + yIntercept();
+            }
+            if (x < 0) {
+                y = -(y);
+                x = Math.abs(x);
+                if (yIntercept() < 0) {
+                    return "y = " + y + "/" + x + "x " + "- " + Math.abs(yIntercept());
+                } else if (yIntercept() == 0) {
+                    return "y = " + y + "/" + x + "x ";
+                }
+                return "y = " + y + "/" + x + "x" + " + " + yIntercept();
+            }
+            if (yIntercept() < 0) {
+                return "y = " + y + "/" + x + "x " + "- " + Math.abs(yIntercept());
+            }
+            if (yIntercept() == 0) {
                 return "y = " + y + "/" + x + "x ";
             }
             return "y = " + y + "/" + x + "x" + " + " + yIntercept();
         }
-        if (yIntercept()<0) {
-            return "y = " + y + "/" + x + "x " +  "- " + Math.abs(yIntercept());
-        }
-        if (yIntercept()==0) {
-            return "y = " + y + "/" + x + "x ";
-        }
-        return "y = " + y + "/" + x + "x" + " + " + yIntercept();
-    }
+        //returns strings of equations that have multiple criterias, and if the coordinates follow it, they will return that specific statement
 
-    public String lineInfo() {
-        String coordinatePt= "The two points are: (" + x1 + "," + y1 + ") " + "and " + "(" + x2 + "," + y2 + ")";
-        String equation= "The equation of the line between these points is: " + equation();
-        String slope= "The slope of the line is: " + slope();
-        String yInt= "The y-intercept of the line is: " + yIntercept();
-        String distance= "The distance between the two points is: " + distance();
-        return coordinatePt + "\n" + equation + "\n" + slope + "\n" + yInt + "\n" + distance + "\n" + "\n";
-    }
+        public String lineInfo () {
+            String coordinatePt = "The two points are: (" + x1 + "," + y1 + ") " + "and " + "(" + x2 + "," + y2 + ")";
+            String equation = "The equation of the line between these points is: " + equation();
+            String slope = "The slope of the line is: " + slope();
+            String yInt = "The y-intercept of the line is: " + yIntercept();
+            String distance = "The distance between the two points is: " + distance();
+            return coordinatePt + "\n" + equation + "\n" + slope + "\n" + yInt + "\n" + distance + "\n" + "\n";
+        }
+        //returns all the info from all these methods that's called in the Runner class
 
-    public String coordinateForX(double newX) {
-        double yVal= slope() * newX + yIntercept();
-        return "The point on the line is: (" + newX+ ", " + yVal+ ")";
+        public String coordinateForX ( double newX){
+            double yVal = slope() * newX + yIntercept();
+            return "The point on the line is: (" + newX + ", " + yVal + ")";
+        }
     }
-}
+    // calculates the y value for the new inputted x value
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -9,23 +9,27 @@ public class LinearEquationRunner {
         String coordinate2= input.nextLine();
         System.out.println();
         System.out.println();
+        // welcoming the user to the program, interaction, inputting points
 
         int x1= Integer.parseInt(coordinate1.substring(1,coordinate1.indexOf(",")));
-        int y1= Integer.parseInt(coordinate1.substring(coordinate1.indexOf(",") + 1, coordinate1.indexOf(")")));
+        int y1= Integer.parseInt(coordinate1.substring(coordinate1.indexOf(",") + 2, coordinate1.indexOf(")")));
         int x2= Integer.parseInt(coordinate2.substring(1,coordinate2.indexOf(",")));
-        int y2= Integer.parseInt(coordinate2.substring(coordinate2.indexOf(",") + 1,coordinate2.indexOf(")")));
-        LinearEquation random= new LinearEquation(x1,y1,x2,y2);
+        int y2= Integer.parseInt(coordinate2.substring(coordinate2.indexOf(",") + 2,coordinate2.indexOf(")")));
+        LinearEquation obj= new LinearEquation(x1,y1,x2,y2);
+        //make the coordinates integers/numbers so they can be used for calculating certain things
         if (x1!=x2) {
-            System.out.print(random.lineInfo());
+            System.out.println("----- Line info -----");
+            System.out.print(obj.lineInfo());
         } else {
             System.out.println("These points are on a vertical line: x = " + x1);
         }
-
+        //if the x coordinates are the same or not, if they are they would report the statement underneath the else portion, if not it is on the if portion
 
         System.out.print("Enter a value for x: ");
         double newX= input.nextDouble();
         System.out.println();
-        System.out.println(random.coordinateForX(newX));
+        System.out.println(obj.coordinateForX(newX));
+        //inputting another x-coordinate to get the y-coordinate
 
 
 
